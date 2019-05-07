aa9dcb79a296660850de9848d43586ec76843127
Mark Thomas <mt_1990@aol.co.uk>
2019-05-07 02:19:13 +0100

    demo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+var stripe = require('stripe')('sk_live_NgaszAbqTWN9bJujiwQufd5q00Jhp5yUh1')
+var bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -6,5 +8,4 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
